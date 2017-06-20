0fbe74631aaa23c413b4b62a8585a9aeecd7370e
kelvinsun(孙承祚) <kelvinsun@tencent.com>
2017-06-20 20:16:26 +0800

    增删改

--- popup.js
+++ popup.js
@@ -46,48 +46,6 @@ document.getElementById('add').addEventListener('click', function () {
     }
 });
 
-function initDOM() {
-    var rules = document.getElementById('rules');
-    var userSettings = JSON.parse(localStorage.getItem('userSettings')) || [];
-    var tpl = '';
-    userSettings.forEach(function (item) {
-        tpl += tplListItem(item);
-    });
-    rules.innerHTML = tpl;
-
-    bindListEvents();
-
-    function bindListEvents() {
-        Array.from(rules.querySelectorAll('li')).forEach(function (item) {
-            var id = item.dataset['id'];
-            item.querySelector('[btn-modify]').addEventListener('click', function () {
-                modifyRule();
-            });
-            item.querySelector('[btn-delete]').addEventListener('click', function () {
-                deleteRule(id);
-            });
-        });
-    }
-}
-
-function tplListItem(values) {
-    values = values || {};
-    return (
-        '<li data-id="${id}">' +
-            '<label>' +
-                '<span>规则</span>' +
-                '<input name="regex" type="text" value="${regex}"/>' +
-            '</label>' +
-            '<label>' +
-                '<span>行为</span>' +
-                '<input name="url" type="text" value="${url}"/>' +
-            '</label>' +
-            '<button btn-modify>编辑</button>' +
-            '<button btn-delete>删除</button>' +
-        '</li>'
-    ).replace(/\${id}/, values['id']).replace(/\${regex}/, values['regex']).replace(/\${url}/, values['url']);
-}
-
 function modifyRule() {
     saveRules(true);
     var insert = document.getElementById('rule_insert');
@@ -120,4 +78,129 @@ function saveRules(avoidInsert) {
         return item;
     });
     localStorage.setItem('userSettings', JSON.stringify(userSettings));
+}
+
+function initDOM() {
+    var rules = document.getElementById('rules');
+    var userSettings = loadFromStore();
+    rules.innerHTML = dataToDOM(userSettings);
+
+    rebindEvent();
+    // var rules = document.getElementById('rules');
+    // var userSettings = JSON.parse(localStorage.getItem('userSettings')) || [];
+    // var tpl = '';
+    // userSettings.forEach(function (item) {
+    //     tpl += tplListItem(item);
+    // });
+    // rules.innerHTML = tpl;
+    //
+    // bindListEvents();
+    //
+    // function bindListEvents() {
+    //     Array.from(rules.querySelectorAll('li')).forEach(function (item) {
+    //         var id = item.dataset['id'];
+    //         item.querySelector('[btn-modify]').addEventListener('click', function () {
+    //             modifyRule();
+    //         });
+    //         item.querySelector('[btn-delete]').addEventListener('click', function () {
+    //             deleteRule(id);
+    //         });
+    //     });
+    // }
+}
+
+function dataFromDOM() {
+    var rules = document.getElementById('rules');
+    var lis;
+    try {
+        lis = Array.from(rules.querySelectorAll('li[item-rules]'));
+    } catch (ex) {
+        // 只有 querySelectorAll 返回 null(也就是没有这种东西) 的时候才会报错
+        lis = [];
+    }
+    return (lis.map(function (item) {
+        return {
+            id: item.dataset['id'],
+            regex: item.querySelector('[name=regex]').value,
+            url: item.querySelector('[name=url]').value,
+        };
+    }) || []).filter(function (item) {
+        return item;
+    });
+}
+
+function dataToDOM(userSettings) {
+    userSettings = userSettings || [];
+    var tpl = '';
+    userSettings.forEach(function (item) {
+        tpl += tplListItem(item);
+    });
+    return tpl;
+}
+
+function loadFromStore() {
+    var userSettings = localStorage.getItem('userSettings');
+    try {
+        userSettings = JSON.parse(userSettings);
+    } catch (ex) {
+        userSettings = [];
+    }
+    if (!Array.isArray(userSettings)) {
+        if ('[object Object]' === userSettings.toString()) {
+            userSettings = [userSettings];
+        } else {
+            userSettings = [];
+        }
+    }
+    return userSettings;
+}
+
+function saveToStore(userSettings) {
+    if (!Array.isArray(userSettings)) {
+        userSettings = [];
+    }
+    userSettings = userSettings.filter(function (item) {
+        return item;
+    });
+    localStorage.setItem('userSettings', JSON.stringify(userSettings));
+}
+
+function rebindEvent() {
+    var lis = document.getElementById('rules').querySelectorAll('li');
+
+    if (lis) {
+        Array.from(lis).forEach(function (item) {
+            var id = item.dataset['id'];
+            var $modify = item.querySelector('[btn-modify]');
+            var $delete = item.querySelector('[btn-delete]');
+
+            $modify.removeEventListener('click');
+            $delete.removeEventListener('click');
+
+            $modify.addEventListener('click', function () {
+                // modifyRule();
+            });
+            $delete.addEventListener('click', function () {
+                // deleteRule(id);
+            });
+        });
+    }
+}
+
+function tplListItem(values) {
+    values = values || {};
+    return (
+        '<li data-id="${id}" item-rules>' +
+            '<label>' +
+                '<span>规则</span>' +
+                '<input name="regex" type="text" value="${regex}"/>' +
+            '</label>' +
+            '<label>' +
+                '<span>行为</span>' +
+                '<input name="url" type="text" value="${url}"/>' +
+            '</label>' +
+            '<button btn-modify>编辑</button>' +
+            '<button btn-delete>删除</button>' +
+        '</li>'
+    ).replace(/\${id}/, values['id']).replace(/\${regex}/, values['regex']).replace(/\${url}/, values['url']);
 }
