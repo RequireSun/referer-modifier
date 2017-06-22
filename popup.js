86c1c36717988fa75867aa20b3aa9932de168e37
kelvinsun(孙承祚) <kelvinsun@tencent.com>
2017-06-22 19:03:34 +0800

    dom modify

--- popup.js
+++ popup.js
@@ -13,18 +13,24 @@ document.getElementById('add').addEventListener('click', function () {
         insert.id = 'rule_insert';
         insert.setAttribute('data-id', '' + Date.now());
         insert.innerHTML = (
+            '<input type="checkbox" name="enabled" checkbox-enabled>' +
             '<label>' +
                 '<span>规则</span>' +
                 '<input name="regex" type="text"/>' +
             '</label>' +
             '<label>' +
                 '<span>行为</span>' +
+                '<select name="method">' +
+                    '<option value="change_referrer" ${checked_change_referrer}>修改 referrer</option>' +
+                    '<option value="add_query_parameter" ${checked_add_query_parameter}>为 query 增加参数</option>' +
+                '</select>' +
                 '<input name="url" type="text"/>' +
             '</label>' +
             '<button btn-confirm>确定</button>' +
             '<button btn-cancel>取消</button>'
         );
         insert.querySelector('[btn-confirm]').addEventListener('click', function () {
+            insert.setAttribute('editing', '');
             insert.setAttribute('item-rules', '');
             insert.id = '';
             saveRules();
@@ -36,6 +42,39 @@ document.getElementById('add').addEventListener('click', function () {
         rules.appendChild(insert);
     }
 });
+
+function domModify(e) {
+    var parent = e.currentTarget.parentNode;
+
+    var $valueMethod = parent.querySelector('[name=method]').dataset['value'];
+    var $valueRegex = parent.querySelector('[name=regex]').dataset['value'];
+    var $valueUrl = parent.querySelector('[name=url]').dataset['value'];
+
+    var $tarMethod = parent.querySelector('[name=method]');
+    var $tarRegex = parent.querySelector('[name=regex]');
+    var $tarUrl = parent.querySelector('[name=url]');
+
+    var $method = document.createElement('select');
+    $method.setAttribute('name', 'method');
+    $method.innerHTML = (
+        '<option value="change_referrer" ' + ('change_referrer' === $valueMethod ? 'selected' : '') + '>修改 referrer</option>' +
+        '<option value="add_query_parameter" ' + ('add_query_parameter' === $valueMethod ? 'selected' : '') + '>为 query 增加参数</option>'
+    );
+
+    var $regex = document.createElement('input');
+    $regex.type = 'text';
+    $regex.name = 'regex';
+    $regex.value = $valueRegex;
+
+    var $url = document.createElement('input');
+    $url.type = 'text';
+    $url.name = 'url';
+    $url.value = $valueUrl;
+
+    $tarMethod.parentNode.replaceChild($method, $tarMethod);
+    $tarRegex.parentNode.replaceChild($regex, $tarRegex);
+    $tarUrl.parentNode.replaceChild($url, $tarUrl);
+}
 
 function modifyRule() {
     saveRules();
@@ -57,21 +96,6 @@ function deleteDecorator(e) {
 function saveRules() {
     var userSettings = dataFromDOM();
     saveToStore(userSettings);
-    // var rules = document.getElementById('rules');
-    // var lis = Array.from(rules.getElementsByTagName('li'));
-    // var userSettings = (lis.map(function (item) {
-    //     if (avoidInsert && 'rule_insert' === item.id) {
-    //         return undefined;
-    //     }
-    //     return {
-    //         id: item.dataset['id'],
-    //         regex: item.querySelector('[name=regex]').value,
-    //         url: item.querySelector('[name=url]').value,
-    //     };
-    // }) || []).filter(function (item) {
-    //     return item;
-    // });
-    // localStorage.setItem('userSettings', JSON.stringify(userSettings));
 }
 
 function initDOM() {
@@ -94,13 +118,23 @@ function dataFromDOM() {
         lis = [];
     }
     return (lis.map(function (item) {
-        return {
-            id: item.dataset['id'],
-            enabled: item.querySelector('[name=enabled]').checked,
-            method: item.querySelector('[name=method]').value,
-            regex: item.querySelector('[name=regex]').value,
-            url: item.querySelector('[name=url]').value,
-        };
+        if (item.getAttribute('editing')) {
+            return {
+                id: item.dataset['id'],
+                enabled: item.querySelector('[name=enabled]').checked,
+                method: item.querySelector('[name=method]').value,
+                regex: item.querySelector('[name=regex]').value,
+                url: item.querySelector('[name=url]').value,
+            };
+        } else {
+            return {
+                id: item.dataset['id'],
+                enabled: item.querySelector('[name=enabled]').checked,
+                method: item.querySelector('[name=method]').dataset['value'],
+                regex: item.querySelector('[name=regex]').dataset['value'],
+                url: item.querySelector('[name=url]').dataset['value'],
+            };
+        }
     }) || []).filter(function (item) {
         return item;
     });
@@ -152,11 +186,13 @@ function rebindEvent() {
             var $delete = item.querySelector('[btn-delete]');
             var $enable = item.querySelector('[checkbox-enabled]');
 
-            $modify.removeEventListener('click', modifyRule);
+            // $modify.removeEventListener('click', modifyRule);
+            $modify.removeEventListener('click', domModify);
             $delete.removeEventListener('click', deleteDecorator);
             $enable.removeEventListener('click', modifyRule);
 
-            $modify.addEventListener('click', modifyRule);
+            // $modify.addEventListener('click', modifyRule);
+            $modify.addEventListener('click', domModify);
             $delete.addEventListener('click', deleteDecorator);
             $enable.addEventListener('click', modifyRule);
         });
@@ -169,15 +205,12 @@ function tplListItem(values) {
             '<input type="checkbox" name="enabled" checkbox-enabled ${enabled}>' +
             '<label>' +
                 '<span>规则</span>' +
-                '<input type="text" name="regex" value="${regex}"/>' +
+                '<span name="regex" data-value="${regex}">${regex}</span>' +
             '</label>' +
             '<label>' +
                 '<span>行为</span>' +
-                '<select name="method">' +
-                    '<option value="change_referrer" ${checked_change_referrer}>修改 referrer</option>' +
-                    '<option value="add_query_parameter" ${checked_add_query_parameter}>为 query 增加参数</option>' +
-                '</select>' +
-                '<input type="text" name="url" value="${url}"/>' +
+                '<span name="method" data-value="${method}">${method_text}</span>' +
+                '<span name="url" data-value="${url}">${url}</span>' +
             '</label>' +
             '<button btn-modify>编辑</button>' +
             '<button btn-delete>删除</button>' +
@@ -194,13 +227,59 @@ function tplListItem(values) {
                     item = values[i] ? 'checked' : '';
                     break;
                 case 'method':
-                    key = 'checked_' + values[i],
-                    item = 'selected';
+                    switch (values[i]) {
+                        case 'change_referrer':
+                            tplTxt = tplTxt.replace(new RegExp('\\${method_text}', 'g'), '修改 referrer');
+                            break;
+                        case 'add_query_parameter':
+                            tplTxt = tplTxt.replace(new RegExp('\\${method_text}', 'g'), '为 query 增加参数');
+                            break;
+                    }
                     break;
             }
             tplTxt = tplTxt.replace(new RegExp('\\${' + key + '}', 'g'), item);
         }
     }
 
-    return tplTxt;//.replace(/\${id}/, values['id']).replace(/\${regex}/, values['regex']).replace(/\${url}/, values['url']);
+    return tplTxt;
+
+    // var tplTxt = (
+    //     '<li data-id="${id}" item-rules>' +
+    //         '<input type="checkbox" name="enabled" checkbox-enabled ${enabled}>' +
+    //         '<label>' +
+    //             '<span>规则</span>' +
+    //             '<input type="text" name="regex" value="${regex}"/>' +
+    //         '</label>' +
+    //         '<label>' +
+    //             '<span>行为</span>' +
+    //             '<select name="method">' +
+    //                 '<option value="change_referrer" ${checked_change_referrer}>修改 referrer</option>' +
+    //                 '<option value="add_query_parameter" ${checked_add_query_parameter}>为 query 增加参数</option>' +
+    //             '</select>' +
+    //             '<input type="text" name="url" value="${url}"/>' +
+    //         '</label>' +
+    //         '<button btn-modify>编辑</button>' +
+    //         '<button btn-delete>删除</button>' +
+    //     '</li>'
+    // );
+    // values = values || {};
+    //
+    // for (var i in values) {
+    //     if (values.hasOwnProperty(i)) {
+    //         var key  = i;
+    //         var item = values[i];
+    //         switch (i) {
+    //             case 'enabled':
+    //                 item = values[i] ? 'checked' : '';
+    //                 break;
+    //             case 'method':
+    //                 key = 'checked_' + values[i];
+    //                 item = 'selected';
+    //                 break;
+    //         }
+    //         tplTxt = tplTxt.replace(new RegExp('\\${' + key + '}', 'g'), item);
+    //     }
+    // }
+    //
+    // return tplTxt;
 }
