b1be7b2123f7295ead870b7e02648d72dd0f06cd
RequireSun <kelvinsen@outlook.com>
2017-06-27 22:58:23 +0800

    continue to webpack

---
+++ src/components/App.vue
@@ -0,0 +1,19 @@
+<template>
+    <ul>
+        <li>123</li>
+        <!--<rule v-for="item in rules"></rule>-->
+    </ul>
+</template>
+
+<script>
+    import Rule from './Rule.vue';
+
+    export default {
+        components: { Rule, },
+        data () {
+            return {
+                rules: [{ id: 1, }, { id: 2, }, { id: 3, },],
+            };
+        },
+    };
+</script>
---
+++ src/components/Rule.vue
@@ -0,0 +1,17 @@
+<template>
+    <li>
+        <input type="checkbox" :checked="enabled"/>
+        123
+    </li>
+</template>
+
+<script>
+    export default {
+        name: 'Rule',
+        data () {
+            return {
+                enabled: true,
+            };
+        },
+    };
+</script>
